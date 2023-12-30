ad36a35ac1f271c459b01019d9f4c780dfbdeb48
dmzn <dmznlin@users.noreply.github.com>
2023-12-30 11:11:05 +0800

    *.常规提交。 +.添加 http 支持 +.添加 wcf 微信支持

--- Feihe_cli/go.mod
+++ Feihe_cli/go.mod
@@ -2,26 +2,43 @@ module Feihe_cli
 
 go 1.21.1
 
-require github.com/dmznlin/znlib-go v0.0.3-0.20221101050743-47e8c06990a6
+require (
+	github.com/bytedance/sonic v1.10.2
+	github.com/danbai225/WeChatFerry-go v1.39.0
+	github.com/dmznlin/znlib-go v0.0.3-0.20221101050743-47e8c06990a6
+	github.com/go-ini/ini v1.66.4
+)
 
 require (
-	github.com/akavel/rsrc v0.10.2 // indirect
+	github.com/Microsoft/go-winio v0.6.1 // indirect
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/chenzhuoyu/base64x v0.0.0-20230717121745-296ad89f973d // indirect
+	github.com/chenzhuoyu/iasm v0.9.0 // indirect
+	github.com/danbai225/go-logs v0.3.2 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/forgoer/openssl v1.2.1 // indirect
-	github.com/go-ini/ini v1.66.4 // indirect
 	github.com/go-redis/redis/v8 v8.11.5 // indirect
+	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/gofrs/uuid v4.2.0+incompatible // indirect
 	github.com/google/go-cmp v0.6.0 // indirect
+	github.com/gorilla/websocket v1.5.1 // indirect
 	github.com/jmoiron/sqlx v1.3.5 // indirect
-	github.com/josephspurrier/goversioninfo v1.4.0 // indirect
+	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
+	github.com/kpango/fastime v1.1.9 // indirect
+	github.com/kpango/glg v1.6.15 // indirect
 	github.com/lestrrat/go-file-rotatelogs v0.0.0-20180223000712-d3151e2a480f // indirect
 	github.com/lestrrat/go-strftime v0.0.0-20180220042222-ba3bf9c1d042 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/rifflock/lfshook v0.0.0-20180920164130-b9218ef580f5 // indirect
 	github.com/shiena/ansicolor v0.0.0-20200904210342-c7312218db18 // indirect
 	github.com/shopspring/decimal v1.3.1 // indirect
-	github.com/sirupsen/logrus v1.8.1 // indirect
-	golang.org/x/sys v0.0.0-20220422013727-9388b58f7150 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
+	github.com/sirupsen/logrus v1.9.0 // indirect
+	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
+	go.nanomsg.org/mangos/v3 v3.4.2 // indirect
+	golang.org/x/arch v0.0.0-20210923205945-b76863e36670 // indirect
+	golang.org/x/mod v0.14.0 // indirect
+	golang.org/x/net v0.19.0 // indirect
+	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/tools v0.16.0 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
 )
--- Feihe_cli/src/main.go
+++ Feihe_cli/src/main.go
@@ -1,10 +1,105 @@
 //go:generate goversioninfo -o main.syso ../res/ver.json
 package main
 
-import "github.com/dmznlin/znlib-go/znlib"
+import (
+	"context"
+	"flag"
+	"fmt"
+	"github.com/dmznlin/znlib-go/znlib"
+	"github.com/go-ini/ini"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"strconv"
+	"syscall"
+	"time"
+)
 
+// 初始化基础库
 var _ = znlib.InitLib(nil, nil)
 
+var cfg = struct {
+	bindIP    string //服务地址
+	bindPort  int    //服务端口
+	erpVerify string //ERP Key
+}{
+	"",
+	80,
+	"erp_cli",
+}
+
+// 载入系统配置
+func loadcfg() {
+	f, err := ini.Load(znlib.Application.ConfigFile)
+	if err != nil {
+		znlib.Error("无法载入配置文件：" + err.Error())
+		return
+	}
+
+	sec := f.Section("config")
+	cfg.bindIP = sec.Key("host").MustString(cfg.bindIP)
+	cfg.bindPort = sec.Key("port").MustInt(cfg.bindPort)
+	cfg.erpVerify = sec.Key("verify").MustString(cfg.erpVerify)
+
+	wechat.host = sec.Key("wcfHost").MustString(wechat.host)
+	wechat.port = sec.Key("wcfPort").MustInt(wechat.port)
+
+	if znlib.StrPos(wechat.host, "//") < 1 {
+		wechat.host = "tcp://" + wechat.host
+		//add protocol
+	}
+}
+
 func main() {
-	znlib.Info("Hello,word")
+	loadcfg()
+	//load config first
+
+	var list bool
+	flag.BoolVar(&list, "list", false, "打印微信联系人 或 群成员列表")
+	flag.Parse()
+
+	if list {
+		wechat.listContact()
+		return
+	}
+
+	//  ---------------------------------------------------------------------------
+	mx := http.NewServeMux()
+	mx.HandleFunc("/cli", hand_FromERP)
+
+	srv := http.Server{
+		Addr:    net.JoinHostPort(cfg.bindIP, strconv.Itoa(cfg.bindPort)),
+		Handler: mx,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			panic(err)
+		}
+	}()
+
+	znlib.Info("服务已启动,主配置：" + fmt.Sprintf("%v", cfg))
+	signalCh := make(chan os.Signal, 1)
+	signal.Notify(signalCh, syscall.SIGINT, syscall.SIGTERM)
+	sig := <-signalCh
+
+	znlib.Info(fmt.Sprintf("Received signal: %v\n", sig))
+	ctxTimeout, cancelTimeout := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancelTimeout()
+
+	if err := srv.Shutdown(ctxTimeout); err != nil {
+		select {
+		case <-ctxTimeout.Done():
+			// 由于达到超时时间服务器关闭，未完成优雅关闭
+			znlib.Warn("timeout of 5 seconds.")
+		default:
+			// 其他原因导致的服务关闭异常，未完成优雅关闭
+			znlib.Warn(fmt.Sprintf("Server shutdown failed: %v\n", err))
+		}
+		return
+	}
+
+	// 正确执行优雅关闭服务器
+	znlib.Info("Server shutdown gracefully")
 }
