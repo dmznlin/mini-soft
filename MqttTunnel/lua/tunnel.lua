c2b39922c54dbdebaecf6acfeb99872137ab021d
dmzn <dmzn@163.com>
2026-03-23 18:23:59 +0800

    *.完善 mqtt-tunnel lua收发逻辑

--- MqttTunnel/lua/tunnel.lua
+++ MqttTunnel/lua/tunnel.lua
@@ -2,17 +2,20 @@
   作者： dmzn@163.com 2026-03-17
   描述： mqtt tunnel server
 -------------------------------------------------------------------------------]]
-local tag       = "tunnel"
+local tag           = "tunnel"
 --mqtt client
-local mqttc     = nil
+local mqttc         = nil
+
 --command utils
-local cmd_utils = require("znlib_cmd"):new()
+local cmd_utils     = require("znlib_cmd"):new()
+cmd_utils.ver_lower = true --验证码小写
 
 -- tunnel config
-local tunnel    = {
+local tunnel        = {
   name = "",    --服务名称
   host = "",    --主机地址
   key = "",     --加密秘钥
+  recv = "",    --接收方
   topic = "",   --协商主题
   cmd = "cmd",  --命令通道
   down = "down" --数据通道
@@ -20,7 +23,7 @@ local tunnel    = {
 
 ---发送数据
 ---@param data string 数据
----@param topic string|nil 主题
+---@param topic string 主题
 ---@param qos number|nil 消息级别
 local function mqtt_send(data, topic, qos)
   if mqttc and mqttc:ready() then --mqtt connected
@@ -57,34 +60,29 @@ sys.taskInit(function ()
   --日志上行计时
   znlib.remote_log_set(os.time())
 
-  --载入配置
-  local cfg = require("znlib_cfg").load_default("mqtt", {})
+  tunnel.name = mqttc.utils.name
+  tunnel.key = mqttc.utils.encrypt
+  tunnel.topic = mqttc.utils.topic
 
-  --替换$ID$
-  utils.table_replace(cfg, "%$id%$", id)
-
-  tunnel.name = cfg.server.name
-  tunnel.key = cfg.server.encrypt
-  tunnel.topic = cfg.server.topic
+  --clear
+  mqttc.utils = nil
 
   if #tunnel.key > 0 then
-    tunnel.key = cmd_utils:des_encrypt(tunnel.key)
+    tunnel.key = cmd_utils:des_decrypt(tunnel.key)
+    cmd_utils.key_msg = tunnel.key
   end
-
-  sys.publish(EventType_MQTT_LOG)
-  --mqtt is ok
 end)
 
 ---------------------------------------------------------------------------------
 --命令字段、别名、顺序定义
 local cmd_order      = {
-  { short = "c", long = "cmd" },     --命令字
-  { short = "s", long = "sender" },  --发送方
-  { short = "p", long = "stamp" },   --流水号
-  { short = "n", long = "srvName" }, --服务器
-  { short = "t", long = "topic" },   --主题
-  { short = "d", long = "data" },    --数据
-  { short = "v", long = "verify" }   --验证
+  { short = "c", long = "cmd" },                --命令字
+  { short = "p", long = "stamp" },              --流水号
+  { short = "s", long = "sender" },             --发送方
+  { short = "r", long = "receiver" },           --接收方
+  { short = "t", long = "topic", omit = true }, --主题
+  { short = "d", long = "data", omit = true },  --数据
+  { short = "v", long = "verify", omit = true } --验证
 }
 
 --命令定义
@@ -100,22 +98,32 @@ local lastStamp      = {}
 local function cmd_ConnResponse()
   local cmd = {
     cmd = CmdConnRep,
-    sender = Device_ID,
     stamp = os.time(),
-    srvName = tunnel.name,
+    sender = Device_ID,
+    receiver = tunnel.recv,
     topic = tunnel.topic,
     data = ""
   }
 
-  mqtt_send(cmd_utils:encode(cmd, cmd_order))
+  mqtt_send(cmd_utils:encode(cmd, cmd_order), tunnel.cmd)
+end
+
+local function cmd_HintMsg(recv, msg)
+  local cmd = {
+    cmd = CmdHintMessage,
+    stamp = os.time(),
+    sender = Device_ID,
+    receiver = recv,
+    data = msg
+  }
+
+  mqtt_send(cmd_utils:encode(cmd, cmd_order), tunnel.cmd)
 end
 
 --处理: 服务器 -> 设备数据
 sys.taskInit(function ()
-  --wait mqtt ok
-  sys.waitUntil(EventType_MQTT_LOG)
-
   while true do
+    ::continue:: --跳转坐标
     local ret, topic, data, id = sys.waitUntil(Status_Mqtt_SubData)
     local cmd = cmd_utils:decode(data, cmd_order)
 
@@ -123,24 +131,41 @@ sys.taskInit(function ()
       goto continue
     end
 
-    log.info(tag, utils.table_to_str(cmd))
-
     --忽略自己发出的指令
     if cmd.sender == Device_ID then
-      return
+      goto continue
     end
 
     --与请求的服务器名称不匹配
-    if cmd.srvName ~= tunnel.name then
-      return
+    if (cmd.receiver ~= Device_ID) and      --接收方不是自己
+        (cmd.cmd ~= CmdConnHost and         --除了服务器连接指令外
+          cmd.receiver ~= tunnel.name) then --连接时的服务器名称
+      goto continue
     end
 
+    --反DDOS: 该时间戳已处理
     local stamp = lastStamp[cmd.cmd]
-    if stamp and os.time() - stamp > 5 then -- 时间戳5秒内失效
-      return
+    if stamp == cmd.stamp then
+      goto continue
     end
 
+    --记录时间戳
+    lastStamp[cmd.cmd] = cmd.stamp
 
-    ::continue:: --跳转坐标
+    -- 时间戳5秒内失效
+    if math.abs(os.time() - cmd.stamp) > 5 then
+      cmd_HintMsg(cmd.sender,
+        string.format("time isn't sync: server(%s) client(%s)", os.date("%H:%M:%S", os.time()),
+          os.date("%H:%M:%S", cmd.stamp)))
+      goto continue
+    end
+
+    if cmd.cmd == CmdConnHost then --连接
+      tunnel.recv = cmd.sender
+      tunnel.host = cmd.data
+    end
+
+    cmd_ConnResponse()
+    log.info(tag, "done")
   end
 end)
